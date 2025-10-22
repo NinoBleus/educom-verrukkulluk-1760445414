14370be621a9db442c07860d8330125a231a0807
Nino <ninobleus@gmail.com>
2025-10-22 14:58:50 +0200

    ingredient class (issue 8)

---
+++ lib/ingredient.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once("lib/artikel.php");
+
+class ingredient {
+
+    private $connection;
+
+    public function __construct($connection) {
+        $this->connection = $connection;
+    }
+
+    public function selecteerIngredientsFromRecipe($recipe_id) {
+
+        $sql = "select * from ingredient where gerecht_id = $recipe_id";
+
+        $result = mysqli_query($this->connection, $sql);
+
+        $ingredients = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $article =  $this->selecteerIngredientArticle($row['artikel_id']);
+            $ingredients[] = array_merge(
+            [
+                'id' => $row['id'],
+                'gerecht_id' => $row['gerecht_id'],
+                'artikel_id' => $row['artikel_id'],
+                'amount' => $row['aantal']
+            ],
+            $article);
+        }
+
+        return $ingredients;
+
+    }
+    
+
+    private function selecteerIngredientArticle($artikel_id){
+        $art = new artikel($this->connection);
+        $ingredientsArticles = $art->selecteerArtikel($artikel_id);
+        return($ingredientsArticles);
+    }
+}
